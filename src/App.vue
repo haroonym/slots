6190744ffa3de147336c76a00bc1dad2d0f35140
haroonym <younas.h03@htlwienwest.at>
2021-10-14 23:35:14 +0200

    Aufgabe 3

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,8 @@
     </Sheet>
   </div> -->
 
-<!-- Aufgabe 2 -->
-  <div id="app">
+  <!-- Aufgabe 2 -->
+  <!-- <div id="app">
     <AppLayout>
       <template v-slot:header>
         I was made with Vue slots!
@@ -25,31 +25,120 @@
         By Robert Baumgartner
       </template>
     </AppLayout>
-  </div>
+  </div> -->
 
   <!-- 
   Antwort auf 2: spielt keine Rolle, da man bei dem slot angibt, 
   ob es ein Footer oder ein Header ist 
   -->
-  
+
+<!-- Aufgabe 3 -->
+  <div id="app">
+    <DataTable :items="desserts" :headers="headers">
+      <template v-slot:header>
+        <h2>Don't Watch What You Eat!</h2> <br>
+      </template>
+      <template v-slot:footer> Created 2021 by Haroon Younas </template>
+      <template v-slot:body_content>
+        <tr class="last-row">
+          <td><b>Total</b></td>
+          <td><b>{{ totalCalories }}</b></td>
+          <td><b>{{ totalFat }}</b></td>
+          <td><b>{{ totalCarbs }}</b></td>
+          <td><b>{{ totalProtein }}</b></td>
+          <td></td>
+        </tr>
+      </template>
+    </DataTable>
+  </div>
 </template>
 
 <script>
-import AppButton from '@/components/AppButton.vue';
+// import AppButton from '@/components/AppButton.vue';
 // import Sheet from '@/components/Sheet.vue';
-import AppLayout from '@/components/AppLayout.vue';
+// import AppLayout from '@/components/AppLayout.vue';
+import DataTable from '@/components/DataTable.vue';
 export default {
   name: 'App',
 
   components: {
-    AppButton,
+    // AppButton,
     // Sheet,
-    AppLayout,
+    // AppLayout,
+    DataTable,
   },
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          value: 'dessert',
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' },
+      ],
+      desserts: [
+        {
+          dessert: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          dessert: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          dessert: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          dessert: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+        {
+          dessert: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%',
+        },
+      ],
+    };
+  },
+  computed: {
+    totalCalories() {
+      return this.desserts.map((el) => el.calories).reduce((p, c) => p + c);
+    },
+    totalFat() {
+      return this.desserts.map((el) => el.fat).reduce((p, c) => p + c);
+    },
+    totalCarbs() {
+      return this.desserts.map((el) => el.carbs).reduce((p, c) => p + c);
+    },
+    totalProtein() {
+      return this.desserts.map((el) => el.protein).reduce((p, c) => p + c);
+    },
+  },
 };
 </script>
 
---
+++ src/components/DataTable.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="data-table">
+    <h2><slot name="header"></slot></h2>
+    <table>
+      <thead>
+        <tr>
+          <th v-for="h of headers" :key="h">{{ h.text }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="i of items" :key="i">
+          <td>{{ i.dessert }}</td>
+          <td>{{ i.calories }}</td>
+          <td>{{ i.fat }}</td>
+          <td>{{ i.carbs }}</td>
+          <td>{{ i.protein }}</td>
+          <td>{{ i.iron }}</td>
+        </tr>
+        <slot name="body_content"></slot>
+      </tbody>
+    </table>
+    <footer><slot name="footer"></slot></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+    },
+    headers: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.data-table {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  margin: 40px;
+  border-collapse: collapse;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.data-table td,
+th {
+  text-align: left;
+}
+.data-table footer {
+  margin: 30px 0 30px 0;
+  font-style: italic;
+}
+.data-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+.data-table th,
+.data-table td {
+  padding: 12px 15px;
+}
+.data-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+.data-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+</style>
